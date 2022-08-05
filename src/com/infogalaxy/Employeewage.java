0ef78d96ebad721775b249923f6d8f5dc6225662
morekalpesh10 <morekalpesh10@gmail.com>
2022-08-05 08:50:06 +0530

    ADDED:[Kalpesh] Employee wage Project Setup

---
+++ src/com/infogalaxy/Employeewage.java
@@ -0,0 +1,11 @@
+package com.infogalaxy;
+
+public class Employeewage {
+
+	public static void main(String[] args) {
+		// Welcome message
+		System.out.println("Welcome to Employee Wage Computation Program");
+
+	}
+
+}
