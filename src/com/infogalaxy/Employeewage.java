fa56971ae37566e74a8a474679ee532d273a6054
morekalpesh10 <morekalpesh10@gmail.com>
2022-08-06 08:55:22 +0530

    MODIFIED:[Kalpesh] Check UC-1-EmployeeAttendance

--- src/com/infogalaxy/Employeewage.java
+++ src/com/infogalaxy/Employeewage.java
@@ -5,7 +5,15 @@ public class Employeewage {
 	public static void main(String[] args) {
 		// Welcome message
 		System.out.println("Welcome to Employee Wage Computation Program");
-
+		
+		// uc-1-check attendance
+		int Check_Attendance = (int)Math.floor(Math.random()*10)%2;
+		if(Check_Attendance==1) {
+			System.out.println("Employee is Present");
+			
+		}else {
+			System.out.println("Employee is absent");
+		}
+		
 	}
-
 }
